ee8de0dbfd9e3a2c64d16aebd14ba6de2440694e
Nicolas <nsabre@kerlan-info.fr>
2021-10-27 16:59:58 +0200

    add default entry list

--- INOVEX/src/app/list-entree/list-entree.component.ts
+++ INOVEX/src/app/list-entree/list-entree.component.ts
@@ -24,7 +24,7 @@ export class ListEntreeComponent implements OnInit {
   public containerDasri : product | undefined;
 
   constructor(private moralEntitiesService : moralEntitiesService, private productsService : productsService) {
-    this.debCode = '3';
+    this.debCode = '20';
     this.moralEntities = [];
     this.listDays = [];
     this.listTotal = [];
